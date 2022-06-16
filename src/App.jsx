10df557a3baefe03413b1faab5df371b5eafe64a
Luxx <lakshrajme@gmail.com>
2022-06-17 03:13:31 +0530

    Done Styling using Sass Css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Board from './compoments/Board';
+import './styles/root.scss';
 
 const App = () => {
   return (
-    <div>
+    <div className="app">
       <h1>Tic-Tak-Toe</h1>
       <Board />
     </div>
