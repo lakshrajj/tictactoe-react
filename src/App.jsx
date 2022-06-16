705f6c82e35a7d3ddfaf982c9d45fb84374cadf0
Luxx <lakshrajme@gmail.com>
2022-06-17 02:46:21 +0530

    Created Board and Box

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
+import Board from './compoments/Board';
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome to React Vite Micro App!</h1>
-      <p>Hard to get more minimal than this React app.</p>
-      <small>Heello</small>
-    </>
+    <div>
+      <h1>Tic-Tak-Toe</h1>
+      <Board />
+    </div>
   );
 };
 
