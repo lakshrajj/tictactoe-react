b1b2490a8360f7bc838a8d804c62b7e5120b1c01
Luxx <lakshrajme@gmail.com>
2022-06-18 08:54:50 +0530

    Added game history

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,50 @@ import { calculateWinner } from './compoments/helpers';
 import './styles/root.scss';
 
 const App = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [currentPLayer, nextPlayer] = useState(false);
+  const [history, setHistory] = useState([
+    { board: Array(9).fill(null), currentPLayer: true },
+  ]);
+  const [currentMove, setCurrentMove] = useState(0);
 
-  const winner = calculateWinner(board);
+  const current = history[currentMove];
+
+  const winner = calculateWinner(current.board);
+
+  console.log(history, current.board);
 
   const message = winner
     ? `Winner is ${winner}`
-    : `Next Player is ${currentPLayer ? 'O' : 'X'}`;
+    : `Next Player is ${current.currentPLayer ? 'O' : 'X'}`;
 
   console.log(winner);
   const handleSquareClick = position => {
-    if (board[position] || winner) {
+    if (current.board[position] || winner) {
       return;
     }
 
-    setBoard(prev => {
-      return prev.map((square, pos) => {
+    setHistory(prev => {
+      const last = prev[prev.length - 1];
+
+      const newBoard = last.board.map((square, pos) => {
         if (pos === position) {
-          return currentPLayer ? 'O' : 'X';
+          return last.currentPLayer ? 'O' : 'X';
         }
         return square;
       });
+      return prev.concat({
+        board: newBoard,
+        currentPLayer: !last.currentPLayer,
+      });
     });
-    nextPlayer(prev => !prev);
+
+    setCurrentMove(prev => prev + 1);
   };
 
   return (
     <div className="app">
       <h1>Tic-Tak-Toe</h1>
       <h2>{message}</h2>
-      <Board board={board} handleSquareClick={handleSquareClick} />
+      <Board board={current.board} handleSquareClick={handleSquareClick} />
     </div>
   );
 };
